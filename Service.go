dba0a0d944eab44a036a0c21397a8b2ef22661bc
caselongo <caselongo@gmail.com>
2022-02-18 15:08:18 +0100

    x

--- Service.go
+++ Service.go
@@ -3,6 +3,7 @@ package googledrive
 import (
 	"fmt"
 
+	errortools "github.com/leapforce-libraries/go_errortools"
 	google "github.com/leapforce-libraries/go_google"
 )
 
@@ -13,24 +14,51 @@ const (
 
 type Service google.Service
 
+func NewServiceWithAccessToken(cfg *google.ServiceWithAccessTokenConfig) (*Service, *errortools.Error) {
+	googleService, e := google.NewServiceWithAccessToken(cfg)
+	if e != nil {
+		return nil, e
+	}
+	service := Service(*googleService)
+	return &service, nil
+}
+
+func NewServiceWithApiKey(cfg *google.ServiceWithApiKeyConfig) (*Service, *errortools.Error) {
+	googleService, e := google.NewServiceWithApiKey(cfg)
+	if e != nil {
+		return nil, e
+	}
+	service := Service(*googleService)
+	return &service, nil
+}
+
+func NewServiceWithOAuth2(cfg *google.ServiceWithOAuth2Config) (*Service, *errortools.Error) {
+	googleService, e := google.NewServiceWithOAuth2(cfg)
+	if e != nil {
+		return nil, e
+	}
+	service := Service(*googleService)
+	return &service, nil
+}
+
 func (service *Service) url(path string) string {
 	return fmt.Sprintf("%s/%s", apiURL, path)
 }
 
-func (service *Service) APIName() string {
+func (service *Service) ApiName() string {
 	return apiName
 }
 
-func (service *Service) APIKey() string {
-	return service.googleService().APIKey()
+func (service *Service) ApiKey() string {
+	return service.googleService().ApiKey()
 }
 
-func (service *Service) APICallCount() int64 {
-	return service.googleService().APICallCount()
+func (service *Service) ApiCallCount() int64 {
+	return service.googleService().ApiCallCount()
 }
 
-func (service *Service) APIReset() {
-	service.googleService().APIReset()
+func (service *Service) ApiReset() {
+	service.googleService().ApiReset()
 }
 
 func (service *Service) googleService() *google.Service {
--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.15
 
 require (
 	github.com/leapforce-libraries/go_errortools v0.0.0-20211119145736-7b9b61c20618
-	github.com/leapforce-libraries/go_google v0.0.0-20220218134916-b824322f36eb
+	github.com/leapforce-libraries/go_google v0.0.0-20220218140632-76dfe7ec065c
 	github.com/leapforce-libraries/go_http v0.0.0-20211202234851-eaff78430048
 )
